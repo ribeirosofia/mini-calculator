f2f22df427c2cac0e73c0d8df2d3c61871f5105c
ribeirosofia <sofia.ribeiro@creditas.com>
2023-07-12 18:14:00 -0300

    feat: implement calcSum

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const calcSum = () => {
+  const inputA = document.getElementById("inputA");
+  const inputB = document.getElementById("inputB");
+  const inputC = document.getElementById("inputC");
+
+  const addBtn = document.getElementById("btn-add");
+  addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    doMath();
+  });
+
+  doMath = () => {
+    const valueA = Number(inputA.value);
+    const valueB = Number(inputB.value);
+
+    if (valueA !== isNaN && valueB !== isNaN) {
+      const sum = valueA + valueB;
+      inputC.value = sum;
+    }
+  };
+
+  captureEnter = () => {
+    document.addEventListener("keypress", (e) => {
+      if (e.keyCode === 13) {
+        doMath();
+      }
+    });
+  };
+
+  captureEnter();
+};
+
+calcSum();
