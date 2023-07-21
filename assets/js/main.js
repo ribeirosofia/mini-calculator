316e674a63e0b90f23f38c156d2c9b68c61b90a0
ribeirosofia <sofia.ribeiro@creditas.com>
2023-07-21 18:45:07 -0300

    feat: implement inputD field

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,56 +2,77 @@ const calculator = () => {
   const inputA = document.getElementById("inputA");
   const inputB = document.getElementById("inputB");
   const inputC = document.getElementById("inputC");
+  const inputD = document.getElementById("inputD");
 
-  const calc = (operation) => {
+  const doMath = () => {
+    let valueA = inputA.value;
+    let valueB = inputB.value;
+    let valueC = inputC.value;
+    let valueD = inputD.value;
+
+    return { valueA, valueB, valueC, valueD };
+  };
+
+  const setOperator = (op) => {
+    const inputD = document.getElementById("inputD");
+    inputD.value = op;
+  };
+
+  const calc = () => {
     const math = doMath();
 
+    if (math.valueD === "") {
+      alert("Por favor, selecione um operador.");
+      return null;
+    }
+
     if (
-      operation !== "-" &&
-      operation !== "*" &&
-      operation !== "/" &&
-      operation !== "+"
+      math.valueD !== "-" &&
+      math.valueD !== "*" &&
+      math.valueD !== "/" &&
+      math.valueD !== "+"
     ) {
       alert("Operador inválido!");
       return;
     }
-
-    if (operation === "/" && math.valueB === "0") {
+    if (math.valueD === "/" && math.valueB === "0") {
       alert("Operação inválida! Divisão por zero não é possível");
-      return;
+      return null;
     }
 
-    inputC.value = eval(math.valueA + operation + math.valueB);
+    const result = eval(math.valueA + math.valueD + math.valueB);
+    return result;
   };
 
   const addBtn = document.getElementById("btn-add");
   addBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    calc("+");
+    setOperator("+");
   });
 
   const minBtn = document.getElementById("btn-min");
   minBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    calc("-");
+    setOperator("-");
   });
 
   const timesBtn = document.getElementById("btn-times");
   timesBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    calc("*");
+    setOperator("*");
   });
 
   const divBtn = document.getElementById("btn-div");
   divBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    calc("/");
+    setOperator("/");
   });
 
   const clearBtn = document.getElementById("btn-clear");
   clearBtn.addEventListener("click", (e) => {
     e.preventDefault();
     clear();
+    console.log("cleared");
   });
 
   const equalsBtn = document.getElementById("btn-equals");
@@ -60,19 +81,22 @@ const calculator = () => {
     equals();
   });
 
-  const doMath = () => {
-    let valueA = inputA.value;
-    let valueB = inputB.value;
+  // const clear = () => {
+  //   const math = doMath();
+  //   math.valueA = "";
+  //   math.valueB = "";
+  //   math.valueC = "";
+  // };
 
-    return { valueA, valueB };
-  };
+  // const equals = () => {
+  //   const math = doMath();
+  //   const operator = math.valueD;
+  //   const result = calc(operator);
 
-  const clear = () => {
-    inputA.value = "";
-    inputB.value = "";
-    inputC.value = "";
-  };
-
-  const equals = () => {};
+  //   if (result !== null) {
+  //     math.valueC = result;
+  //   }
+  // };
 };
+
 calculator();
